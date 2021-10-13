8f43c71fc171f267fc21b6c032f134bac9cfde00
hackurity01 <hackurity01@gmail.com>
2021-10-13 22:30:34 +0900

    init

---
+++ 1-mr-blog/js/index.js
@@ -0,0 +1,21 @@
+/**
+ * 저장 버튼 클릭 함수
+ */
+function handleSave() {}
+
+/**
+ * 오른쪽 화살표 버튼 클릭 함수
+ */
+function handleNext() {}
+
+/**
+ * 왼쪽 화살표 버튼 클릭 함수
+ */
+function handlePrev() {}
+
+/**
+ * 서비스 초기화 함수
+ */
+function init() {}
+
+init();
