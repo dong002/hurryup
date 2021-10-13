e5b1802855cd937fb99db85c52e58cf63b9ef6f2
hackurity01 <hackurity01@gmail.com>
2021-10-14 00:14:08 +0900

    2주차 실습 코드 추가

---
+++ 2-graph/js/index.js
@@ -0,0 +1,26 @@
+/**
+ * 옵션(gender, age, questionNo)이 주어지면, 해당하는 그래프를 그리는 함수
+ *
+ * @ param {string} gender 선택된 성별
+ * @ param {number} age 선택된 나이
+ * @ param {number} questionNo 선택된 문항
+ */
+function drawGraph(gender, age, questionNo) {}
+
+/**
+ * 답변 별로 조건(age, gender)에 맞는 데이터의 수를 계산하는 함수
+ *
+ * @ param {string} gender 선택된 성별
+ * @ param {number} age 선택된 나이
+ * @ param {number} questionNo 선택된 문항
+ * @ return {object} 답변 별, 데이터 수 ex - {a: 300, b: 100, c: 230}
+ */
+function countData(gender, age, questionNo) {}
+
+/**
+ * 초기화 함수
+ * surveyData를 통해 옵션 버튼 생성
+ */
+function init() {}
+
+init();
