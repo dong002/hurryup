e6373f62d3c518dcf59b1e94eb508931a0d4ba95
hackurity01 <hackurity01@gmail.com>
2021-10-19 00:13:15 +0900

    2주차 보일러 플레이트 추가

--- 2-graph/js/index.js
+++ 2-graph/js/index.js
@@ -1,21 +1,21 @@
-/**
- * 옵션(gender, age, questionNo)이 주어지면, 해당하는 그래프를 그리는 함수
- *
- * @ param {string} gender: 선택된 성별
- * @ param {number} age: 선택된 나이
- * @ param {number} questionNo: 선택된 문항
- */
-function drawGraph(gender, age, questionNo) {}
+// 현재 선택된 옵션 상태 데이터
+const options = {
+  gender: undefined,
+  age: undefined,
+  questionNo: undefined,
+};
 
 /**
- * 답변 별로 조건(age, gender)에 맞는 데이터의 수를 계산하는 함수
+ * 선택된 옵션에 해당하는 그래프를 그리는 함수
+ */
+function drawGraph() {}
+
+/**
+ * 답변 별로 조건(age, gender, questionNo)에 맞는 데이터의 수를 계산하는 함수
  *
- * @ param {string} gender: 선택된 성별
- * @ param {number} age: 선택된 나이
- * @ param {number} questionNo: 선택된 문항
  * @ return {object} 답변 별, 데이터 수 ex - {a: 300, b: 100, c: 230}
  */
-function countData(gender, age, questionNo) {}
+function countData() {}
 
 /**
  * 초기화 함수
